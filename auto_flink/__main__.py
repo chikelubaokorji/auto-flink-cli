6f2677969861e8a50c96ff48513275c099941afa
Fortune Okorji <chikeluba.okorji@gmail.com>
2022-11-19 02:07:50 +0100

    Added find-replace placeholder functionality to both bash and yaml files.

--- auto_flink/__main__.py
+++ auto_flink/__main__.py
@@ -1,9 +1,14 @@
-from .auto_flink import download_repo, explode_dir
+from .auto_flink import *
 
 
 def main():
     d_path = download_repo()
     arr = explode_dir(d_path)
+    for i in arr:
+        if '.sh' in i[-len('.sh')::]:
+            replace_placeholder_sh(i)
+        elif '.yaml' in i[-len('.yaml')::]:
+            replace_placeholder_yaml(i)
 
 
 if __name__ == '__main__':
--- auto_flink/auto_flink.py
+++ auto_flink/auto_flink.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 
 
@@ -20,6 +21,55 @@ def explode_dir(d_path):
     for root, _, files in os.walk(d_path):
         for f in files:
             path = "{}/{}".format(root, f)
-            if not path.__contains__(".git"):
+            if not ('/.git/' in path):
                 arr_path.append(path)
     return arr_path
+
+
+def find_replace_yaml(file_path):
+    print("Editing YAML File: %s" % file_path)
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        for i in range(0, len(lines)):
+            p = re.compile(r'(.*?):\s\$\{')
+            param = p.findall(lines[i])
+            if not len(param) == 0:
+                replace_str = str(input("Enter value for %s:\n" % param[0]))
+                regex = r'\$\{.*?}'
+                new_line = re.sub(regex, replace_str, lines[i])
+                lines[i] = new_line
+        f.close()
+    with open(file_path, 'w') as f:
+        f.writelines(lines)
+        print("Closing file: %s\n" % file_path)
+        f.close()
+
+
+def replace_placeholder_sh(file_path):
+    print("Editing Bash File: %s" % file_path)
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        for i in range(0, len(lines)):
+            regex = r'(?:\:\-\$\{.*?\}\})|(?:\$\{(.*?)})'
+            p = re.compile(regex)
+            param = p.findall(lines[i])
+            if not len(param) == 0:
+                print("\nEditing placeholders in line: %s" % lines[i])
+                for j in range(0, len(param)):
+                    if ':-${' in param[j]:
+                        replace_str = str(input("Enter value for %s:\n" % param[j].split(':-${')[1])).replace(" ", "")
+                        lines[i] = lines[i].replace('${' + param[j].split(':-${')[1] + '}', replace_str)
+                    else:
+                        if ':-' in param[j]:
+                            pass
+                            # Do something
+                        else:
+                            print(param[j])
+                            replace_str = str(input("Enter value for %s:\n" % param[j])).replace(" ", "")
+                            lines[i] = lines[i].replace('${' + param[j] + '}', replace_str)
+                            print(lines[i])
+        f.close()
+    with open(file_path, 'w') as f:
+        f.writelines(lines)
+        print("Closing file: %s\n" % file_path)
+        f.close()
